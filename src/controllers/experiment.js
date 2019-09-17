bf21c96fa2837d521549c3e81c58c07230132b11
Yu-Hsin "Ethan" Lin <yuhsin.lin@outlook.com>
2019-09-17 20:46:31 +0800

    Set Timezone in Dockerfile & Fix try-catch while target exists

--- src/controllers/experiment.js
+++ src/controllers/experiment.js
@@ -2,10 +2,17 @@ const ExperimentModel = require('../models/experiment');
 
 module.exports = {
   createSingle: async (ctx) => {
-    await ExperimentModel.createSingle(ctx.request.body);
+    try {
+      await ExperimentModel.createSingle(ctx.request.body);
+    } catch (err) {
+      if (err.code === 'ConditionalCheckFailedException') {
+        ctx.throw(409, 'Target Already Exists');
+      }
+    }
+    ctx.status = 201;
     ctx.body ={
-      'statusCode': 200,
-      'message': 'Uploaded',
+      'statusCode': 201,
+      'message': 'Created',
     };
   },
   getList: async (ctx) => {
